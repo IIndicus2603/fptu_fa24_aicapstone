efc443bc95c148cb3e1af2a0f492bcf7304f8008
IIndicus2603 <505error260303@gmail.com>
2024-11-17 21:28:58 +0700

    Update urls.py

--- web app/photo_slider/urls.py
+++ web app/photo_slider/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('', views.image_slider, name='image_slider'),  # Ensure 'image_slider' is the correct name
     path('images/upload/', views.image_upload, name='image_upload'),
     path('images/check_duplicate/', views.image_check_duplicate, name='image_check_duplicate'),
-
+    path('images/update_tag/', views.update_tag, name='update_tag'),
 ]
 
 # Cung cấp các file media trong môi trường phát triển
