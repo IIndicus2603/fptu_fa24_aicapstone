2791bf526d7638fb6fcfd575366c43defcf7c6a9
IIndicus2603 <505error260303@gmail.com>
2024-11-17 21:28:20 +0700

    Update views.py

--- web app/images/views.py
+++ web app/images/views.py
@@ -7,7 +7,6 @@ from django.http import JsonResponse
 from .models import Image
 from .forms import ImageForm
 from django.http import JsonResponse
-import logging
 
 def image_upload(request):
     if request.method == 'POST' and request.FILES.get('image'):
@@ -53,4 +52,44 @@ def image_check_duplicate(request):
         
         return JsonResponse({'exists': exists})
 
+def update_tag(request):
+    if request.method == 'POST':
+        tag_file_path = os.path.join(settings.BASE_DIR, 'tag.json')
+
+        image_path = request.POST.get('image_name')  # Image name
+        old_tag = request.POST.get('old_tag')        # Old tag
+        new_tag = request.POST.get('new_tag')        # New tag
+        image_name = os.path.basename(image_path)
+        if not (image_name and old_tag and new_tag):
+            return JsonResponse({'success': False, 'error': 'Missing data'}, status=400)
+
+        # Read the tag.json file
+        try:
+            with open(tag_file_path, 'r', encoding='utf-8') as file:
+                tags = json.load(file)
+        except FileNotFoundError:
+            return JsonResponse({'success': False, 'error': 'tag.json file not found'}, status=404)
+        except json.JSONDecodeError:
+            return JsonResponse({'success': False, 'error': 'Invalid JSON in tag.json'}, status=500)
+
+        # Check if the image exists in the tag data
+        if image_name not in tags:
+            return JsonResponse({'success': False, 'error': 'No tags found for this image in tag.json'}, status=400)
+
+        # Check if old_tag exists in the image's tags
+        if old_tag in tags[image_name]:
+            tags[image_name] = [new_tag if tag == old_tag else tag for tag in tags[image_name]]
+
+            # Save the updated tags to tag.json
+            try:
+                with open(tag_file_path, 'w', encoding='utf-8') as file:
+                    json.dump(tags, file, ensure_ascii=False, indent=4)
+            except Exception as e:
+                return JsonResponse({'success': False, 'error': str(e)}, status=500)
+
+            return JsonResponse({'success': True, 'tags': tags[image_name]})
+        else:
+            return JsonResponse({'success': False, 'error': 'Old tag not found'}, status=400)
+
+    return JsonResponse({'success': False}, status=400)
 
