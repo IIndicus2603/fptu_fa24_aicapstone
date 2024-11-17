8c26a0f0348622b60d63f9fecc9b3860526e66c2
IIndicus2603 <505error260303@gmail.com>
2024-11-17 21:28:40 +0700

    Update urls.py

--- web app/images/urls.py
+++ web app/images/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.image_slider, name='image_slider'),  # Ensure 'image_slider' is the correct name
     path('images/upload/', views.image_upload, name='image_upload'),
     path('images/check_duplicate/', views.image_check_duplicate, name='image_check_duplicate'),
-
+    path('images/update_tag/', views.update_tag, name='update_tag'),
 ]
