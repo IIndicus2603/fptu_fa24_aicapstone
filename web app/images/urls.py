15800f5f838ddfcd16dbe48e6d7b569d5406385b
IIndicus2603 <505error260303@gmail.com>
2024-11-18 21:05:58 +0700

    Update urls.py

--- web app/images/urls.py
+++ web app/images/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('images/upload/', views.image_upload, name='image_upload'),
     path('images/check_duplicate/', views.image_check_duplicate, name='image_check_duplicate'),
     path('images/update_tag/', views.update_tag, name='update_tag'),
+    path('add_tag/', views.add_tag, name='add_tag'),  # Thêm dòng này
 ]
